5bf8ef6f7e5f74da1321443bf4e248370d231cba
navinate <treycluff@gmail.com>
2024-02-20 21:05:18 -0500

    updated colors of orbs

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,5 +1,5 @@
 const words =     ['About Me',  'Shaders',    'Web Apps', 'StairWELL',   'School\nProjects', 'Personal\nArt'];
 const links =     ['./content/about',   './content/shaders', './content/webapps', './content/stairwell', './content/schoolwork', './content/art'];
-const colors =    ['#2A9D8F',   '#E9C46A',   '#F4A261',   '#E76F51',     '#264653',          '#239DAF'];
+const colors =    ['#7CB518',   '#8231C4',   '#009DDC',   '#EC0B43',     '#264653',          '#239DAF'];
 
 export { words, links, colors };
